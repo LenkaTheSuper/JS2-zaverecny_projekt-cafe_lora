26a0fea60b7d50017ef1b25bd22475c57e171be5
Lenka Michaleová <michaleova.lenka@gmail.com>
2024-07-02 19:35:11 +0200

    vytvoreni komponenty Layer, pouziti destrukturovani, zobrazeni v komponente Drink

--- src/components/3-1_Drink/Drink.jsx
+++ src/components/3-1_Drink/Drink.jsx
@@ -1,24 +1,22 @@
 import './Drink.css';
+import { Layer } from '../3-2_Layer/Layer';
 
 export const Drink = ({id, name, ordered, image, layers}) => (
     <div className="drink">
         <div className="drink__product">
-        <div className="drink__cup">
-            <img src={image} />
-        </div>
-        <div className="drink__info">
-            <h3>{name}</h3>
-            <div className="layer">
-            <div className="layer__color" style={{ backgroundColor: '#613916' }}></div>
-            <div className="layer__label">espresso</div>
+            <div className="drink__cup">
+                <img src={image} />
+            </div>
+            <div className="drink__info">
+                <h3>{name}</h3>
+                    <Layer color="#feeeca" label="mléčná pěna" />
             </div>
         </div>
-        </div>
         <form className="drink__controls">
-        <input type="hidden" className="order-id" value="0" />
-        <button className="order-btn">
-            Objednat
-        </button>
+            <input type="hidden" className="order-id" value="0" />
+            <button className="order-btn">
+                Objednat
+            </button>
         </form>
     </div>
 )
